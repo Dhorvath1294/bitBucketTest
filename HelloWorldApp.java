4dd9769c6361fc1ac8f745b3cc3273cce9c3d3e9
Dhorvath1294 <48832842+Dhorvath1294@users.noreply.github.com>
2021-03-24 13:08:54 +0100

    fourth commit

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -2,4 +2,8 @@ class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
     }
+    
+    public int addInt(int a, int b) {
+        return a+b;
+    }
 }
