dcac81cac8cfc7e23caf08a9a3f1fc1d0dc9218c
Dhorvath1294 <48832842+Dhorvath1294@users.noreply.github.com>
2021-03-24 10:53:55 +0100

    second commit

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -2,4 +2,12 @@ class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
     }
+    
+    public void testFtn1() {
+        System.out.println("");
+    }
+    
+    public void testFtn2() {
+           
+    }
 }
