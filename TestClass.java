a32a5911b54b92fcfaece1feb51f15d7507961e5
Dhorvath1294 <48832842+Dhorvath1294@users.noreply.github.com>
2021-03-25 10:01:09 +0100

    Create TestClass.java

---
+++ TestClass.java
@@ -0,0 +1,13 @@
+public class TestClass {
+    public void testFtn4() {
+        System.out.println("");
+    }
+    
+    public void testFtn5() {
+           
+    }
+    
+    public void testFtn6() {
+       String s;    
+    }
+}
